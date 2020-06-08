a49bf03c286155c156f60af48789b7907445f6ce
Harry <hw00441@surrey.ac.uk>
2020-06-08 13:59:35 +0100

    added asynchronous emails for password resets

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -13,7 +14,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
+mail = Mail(app)
+##pip install eventlet?????
 
 if not app.debug:
 	if app.config['MAIL_SERVER']:
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from app import db, login
+from time import time
+import jwt
+from app import db, login, app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
@@ -67,11 +69,19 @@ class User(UserMixin, db.Model):
 	def get_friends(self):
 		return set(self.followed) & set(self.followers)
 	
-	def get_followed(self):
-		return set(self.followed)
+	def get_reset_password_token(self, expires_in=600):
+		return jwt.encode(
+			{'reset_password': self.id, 'exp': time() + expires_in},
+			app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 	
-	def get_followers(self):
-		return set(self.followers)
+	@staticmethod
+	def verify_reset_password_token(token):
+		try:
+			id = jwt.decode(token, app.config['SECRET_KEY'],
+					algorithms=['HS256'])['reset_password']
+		except:
+			return
+		return User.query.get(id)
 		
 
 
