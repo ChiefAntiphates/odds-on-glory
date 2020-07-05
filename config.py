15bbcf8cd962c922a2002c07885dbf4c91443f9c
Harry <hw00441@surrey.ac.uk>
2020-07-05 08:56:17 +0100

    minor change in timing

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'artichokey-nono'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-		'sqlite:///' +  os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or \
+		#'sqlite:///' +  os.path.join(basedir, 'app.db')##OOK
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	
 	MAIL_SERVER = 'smtp.googlemail.com'
