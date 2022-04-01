0252ffd8a9d033eed4d038e353c175d3e4b962ca
Chief Antiphates <harryrdm@gmail.com>
2022-04-01 21:25:48 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'artichokey-nono'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' +  os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = '587'
+	MAIL_USE_TLS = '1'
+	MAIL_USERNAME = 'noreply.oddsonglory@gmail.com'
+	MAIL_PASSWORD = 'D5WAtTiC39e28az'
+	ADMINS = ['noreply.oddsonglory@gmail.com', 'oddsonglory@gmail.com']
+	
+	POSTS_PER_PAGE = 10
+	
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+	#LANGUAGES = ['en', 'es']
